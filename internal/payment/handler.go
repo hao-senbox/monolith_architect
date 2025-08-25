9cdb7cf3e64a29d2dcae970ecdcf9d800b9acaa9
hao-senbox <mr.hao.anh@senbox.org>
2025-08-26 00:35:14 +0700

    push code

--- internal/payment/handler.go
+++ internal/payment/handler.go
@@ -98,7 +98,7 @@ func (h *PaymentHandler) HandleVNPayCallback(c *gin.Context) {
 	}
 
 	var path string
-	pathOrigin := "http://localhost:5173"
+	pathOrigin := "https://shimmering-faun-1418f7.netlify.app/"
 	switch callback.ResponseCode {
 	case "00":
 		path = "/payment/success"
